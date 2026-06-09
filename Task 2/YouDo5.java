f3d30c7f4d477157b34a53e800b77e0aba597b70
Halima <halimajahanchowdhury@gmail.com>
2026-06-09 10:53:12 +0600

    Update YouDo5.java

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,19 @@
+class Printer {
+    void printData(String text) {
+        System.out.println(text);
+    }
+
+    void printData(int number) {
+        System.out.println(number);
+    }
+}
+
+public class Main4 {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.printData("Hello World!");
+        printer.printData(100);
+    }
+
+}
 
