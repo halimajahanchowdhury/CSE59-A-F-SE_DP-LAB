506467f5977e1c602d6ac16564a93a7434239178
Halima <halimajahanchowdhury@gmail.com>
2026-06-09 11:02:15 +0600

    Update YouDo7.java

--- Task 2/YouDo7.java
+++ Task 2/YouDo7.java
@@ -1,1 +1,20 @@
+interface Clickable {
+    void onClick();
+
+}
+
+class Button implements Clickable {
+    @Override
+    public void onClick() {
+        System.out.println("Button was clicked!");
+    }
+}
+
+public class Main6 {
+    public static void main(String[] args) {
+        Button button = new Button();
+        button.onClick();
+    }
+
+}
 
