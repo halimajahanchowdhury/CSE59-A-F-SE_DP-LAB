f23d4fb3beb736dae176a801d579bfe8b23bafed
Halima <halimajahanchowdhury@gmail.com>
2026-06-09 10:42:45 +0600

    Update YouDo3.java

--- Task 2/YouDo3.java
+++ Task 2/YouDo3.java
@@ -1,1 +1,18 @@
+class CountDown {
+    void start() {
+        for (int i = 5; i >= 1; i--)
+        {
+            System.out.println(i);
+        }
+        System.out.println("Go!");
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        CountDown countdown = new CountDown();
+        countdown.start();
+    }
+
+}
 
