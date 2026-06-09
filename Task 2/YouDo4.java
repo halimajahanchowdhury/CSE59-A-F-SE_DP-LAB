8bb9d668f86497bf75f6cb9988913f26e4ddcd2a
Halima <halimajahanchowdhury@gmail.com>
2026-06-09 10:48:50 +0600

    Update YouDo4.java

--- Task 2/YouDo4.java
+++ Task 2/YouDo4.java
@@ -1,1 +1,24 @@
+class Sensor {
+    String name;
+    int BatteryLevel;
+
+    Sensor(String name, int BatteryLevel) {
+        this.name = name;
+        this.BatteryLevel = BatteryLevel;
+    }
+
+    void charge() {
+        BatteryLevel = 100;
+        System.out.println("Sensor fully charged!");
+    }
+}
+
+public class Main3 {
+    public static void main(String[] args) {
+        Sensor sensor = new Sensor("Temperature Sensor", 50);
+        sensor.charge();
+        System.out.println("Battery Level: " + sensor.BatteryLevel);
+    }
+
+}
 
