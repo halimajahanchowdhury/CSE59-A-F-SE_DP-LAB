4276a9acd4eb950cc18ebe253ac78a06e31b8ac2
Halima <halimajahanchowdhury@gmail.com>
2026-06-22 21:57:43 -0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,39 @@
+public class Main2 {
+    public static void main (String[] args) {
+        Employee employee = new Employee("Halima");
+        Contractor contractor = new Contractor("Alex");
+        employee.calculateBonus();
+        contractor.work();
+    }
+}
 
+class TeamMember {
+    protected String name;
+
+    TeamMember(String name){
+        this.name = name;
+    }
+}
+
+interface BonusEligible{
+    void calculateBonus ();
+} 
+
+class Employee extends TeamMember implements BonusEligible{
+    Employee(String name) {
+        super(name);
+    }
+    @Override
+    public void calculateBonus(){
+        System.out.println("Calculating employee bonus...");
+    }
+}
+
+class Contractor extends TeamMember {
+    Contractor(String name){
+        super(name);
+    }
+    public void work(){
+        System.out.println("Contractor working..");
+    }
+}
