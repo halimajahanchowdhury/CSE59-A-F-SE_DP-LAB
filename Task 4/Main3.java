7463b032b6adead2124afc39d44dcff0f531434e
Halima <halimajahanchowdhury@gmail.com>
2026-06-22 22:12:19 -0600

    Update Main3.java

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,52 @@
+public class Main3 {
+    public static void main(String[] args) {
+        BasicPrinter printer = new BasicPrinter();
+
+        printer.printDocument();
+
+        MultiFunctionCopier copier = new MultiFunctionCopier();
+
+        copier.printDocument();
+        copier.scanDocument();
+        copier.faxDocument();
+
+    }
+}
+
+interface Printer {
+    void printDocument();
+}
+
+interface Scanner {
+    void scanDocument();
+}
+
+interface FaxMachine {
+    void faxDocument();
+}
+
+class BasicPrinter implements Printer {
+    @Override
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+}
+
+class MultiFunctionCopier
+        implements Printer, Scanner, FaxMachine {
+    @Override
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+
+    @Override
+    public void scanDocument() {
+        System.out.println("Scanning...");
+    }
+
+    @Override
+    public void faxDocument() {
+        System.out.println("Faxing...");
+    }
+}
 
