5dd797c137f9ecc63a0ed84d9c0b129607918255
Halima <halimajahanchowdhury@gmail.com>
2026-06-29 21:57:01 -0600

    Update Main3.java

--- Task 5/Main3.java
+++ Task 5/Main3.java
@@ -1,1 +1,50 @@
+// Patient class
+class Patient {
+    private String patientName;
+    private String nationalID;
 
+    public Patient(String patientName, String nationalID) {
+        this.patientName = patientName;
+        this.nationalID = nationalID;
+    }
+
+    public String getpatientName() {
+        return patientName;
+    }
+
+    public String getnationalID() {
+        return nationalID;
+    }
+}
+
+// IDvalidator class
+
+class IdValidator {
+
+    public static boolean validateId(String nationalID) {
+        return nationalID.length() == 10 || nationalID.length() == 17;
+    }
+}
+
+// SmsService class
+
+class SmsService {
+    public void sendSmsConfirmation(Patient patient) {
+        if (IdValidator.validateId(patient.getnationalID())) {
+            System.out.println("Sending SMS to " + patient.getpatientName() + ": Registration successful.");
+
+        } else {
+            System.out.println("Invalid National ID. SMS not sent.");
+        }
+    }
+}
+
+// main class
+public class Main {
+    public static void main(String[] args) {
+        Patient patient = new Patient("Halima", "1234567890");
+
+        SmsService smsService = new SmsService();
+        smsService.sendSmsConfirmation(patient);
+    }
+}
