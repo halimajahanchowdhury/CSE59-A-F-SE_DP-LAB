5344fad23b4b6b814f4e1f52cc28b4abc26ccd2b
Halima <halimajahanchowdhury@gmail.com>
2026-06-29 22:17:48 -0600

    Update Main4.java

--- Task 5/Main4.java
+++ Task 5/Main4.java
@@ -1,1 +1,48 @@
+// Interface
 
+interface OptimizableModel {
+    void quantize();
+}
+
+// Resnet18 class
+
+class Resnet18 implements OptimizableModel {
+
+    @Override
+
+    public void quantize() {
+        System.out.println("Applying int8 quantization for Resnet-18.");
+    }
+}
+
+// MobileNet class
+
+class MobileNet implements OptimizableModel {
+
+    @Override
+
+    public void quantize() {
+        System.out.println("Applying dynamic quantization for MobileNet.");
+    }
+}
+
+// InferenceEngine class
+class InferenceEngine {
+    public void optimizeModel(OptimizableModel model) {
+        model.quantize();
+    }
+}
+
+// Main class
+
+public class Main {
+    public static void main(String[] args) {
+        InferenceEngine engine = new InferenceEngine();
+
+        OptimizableModel resnet = new Resnet18();
+        OptimizableModel mobilenet = new MobileNet();
+
+        engine.optimizeModel(resnet);
+        engine.optimizeModel(mobilenet);
+    }
+}
