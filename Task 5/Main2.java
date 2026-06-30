16febd87814c304967cd78612b7f16da376deb5c
Halima <halimajahanchowdhury@gmail.com>
2026-06-29 21:36:51 -0600

    Update Main2.java

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,35 @@
+public class Main4 {
 
+    public static void main(String[] args) {
+        MessageSender sender = new EmailSender();
+        NotificationService service = new NotificationService(sender);
+
+        service.alertUser("Hello User!");
+
+    }
+
+}
+
+interface MessageSender {
+    void sendMessage(String message);
+
+}
+
+class EmailSender implements MessageSender {
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+class NotificationService {
+    private MessageSender sender;
+
+    NotificationService(MessageSender sender) {
+        this.sender = sender;
+    }
+
+    public void alertUser(String msg) {
+        sender.sendMessage(msg);
+    }
+}
